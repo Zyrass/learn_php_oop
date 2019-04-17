ec1aca058df0dda0ed687f9ea20cd7634d62e150
Alain Guillon <zyrass@outlook.fr>
2019-04-17 23:43:28 +0200

    optimisation du constructeur

--- 01_introduction/constructor_destructor.php
+++ 01_introduction/constructor_destructor.php
@@ -3,21 +3,24 @@
 class Voiture {
 
 	public $nombre_de_roues = 4;
-	static $nombre_de_porte = 0;
+	static $identitiant = 69000;
 
 	// Constructeur
 	function __construct(){
 
 		// Le mot clé self correspont à la classe Voiture.
-		// Il se traduit soi (soi-même). 
-		echo $this->nombre_de_roues . ' roues. (constructeur) <br />';
-		echo self::$nombre_de_porte++ . ' portes. (constructeur) <br />';
+		// Il se traduit soi (soi-même).
+		// Ici dans le constructeur on l'incrémente ainsi lorsque l'on va
+		// instancier une nouvelle Voiture, celle-ci aura un identifiant incrémenté.
+		
+		//echo $this->nombre_de_roues . ' roues. (constructeur) <br />';
+		echo 'Identifiant N°' . self::$identitiant++ . ' - (constructeur) <br />';
 	}
 
 	// Destructeur
 	function __destruct(){
-		echo $this->nombre_de_roues . ' roues. (déconstructeur) <br />';
-		echo self::$nombre_de_porte-- . ' portes. (déconstructeur) <br />';
+		//echo $this->nombre_de_roues . ' roues. (déconstructeur) <br />';
+		echo 'Identifiant N°' . self::$identitiant-- . ' - (déconstructeur) <br />';
 	}
 
 	// méthode
@@ -27,7 +30,16 @@ class Voiture {
 
 }
 
+echo '<h4>Constructeur</h4>';
 $bmw = new Voiture();
-//$merceds = new Voiture();
-//$merceds_2 = new Voiture();
+$bmw_2 = new Voiture();
+$bmw_3 = new Voiture();
+echo '<br /><hr>';
+echo '<p>On remarquera que le "Destructeur" s\'exécute tout à la fin. Vu que notre
+dernier objet qui à été instancié, disposait de l\'identifiant : 69002.
+Ainsi lors de la décrémentation grâce au "Destructeur", l\'identifiant 
+commencera bien par : 69003.</p>';
+echo '<h4>Méthode</h4>';
 $bmw->details();
+echo '<br /><hr>';
+echo '<h4>Déconstructeur</h4>';
